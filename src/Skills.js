c5618c95b9f5d1f360fdb90004ff17b82f4851db
minsung521 <mingsung521@gmail.com>
2021-08-10 03:41:16 +0900

    skill bar making

--- src/Skills.js
+++ src/Skills.js
@@ -30,8 +30,27 @@ function Skills() {
 					<span ref={skills} className="title"></span>
 				</div>
 				<div className="Skills-contents-main">
-					<p className="Skills-article"></p>
-					<div className="Skills-detail"></div>
+					<p className="Skills-article">
+						<br />
+						<br />
+						1년 전부터 개발자가 되기 위한 공부를 시작한 이후로 저는 클론코딩,
+						토이 프로젝트를 진행하고, 각 분야의 재능있는 사람들과 협력하며
+						경험을 쌓았습니다.
+						<br />
+						<br />
+						풀스택 개발자를 지향하며 Flutter와 같은 앱 개발 프레임워크, 딥러닝을
+						비롯한 머신러닝기술에 관심이 많습니다!
+					</p>
+					<div className="Skills-detail">
+						<li>
+							<div className="Skill-bar">
+								<span>React</span>
+								<footer>
+									<div Style="width:40%"></div>
+								</footer>
+							</div>
+						</li>
+					</div>
 				</div>
 			</div>
 		</div>
