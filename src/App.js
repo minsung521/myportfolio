a2712a3d79f85320f5e3c2d846ef2a1f440850a6
minsung521 <mingsung521@gmail.com>
2021-08-23 02:43:49 +0900

    mobile page will be added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import About from "./About";
 import Skills from "./Skills";
 import Work from "./Work";
 import Contact from "./Contact";
+import "./App.css";
 
 const App = () => {
 	return (
-		<div>
-			<Route path="/" component={Landing} exact />
-			<Route path="/about" component={About} />
-			<Route path="/skills" component={Skills} />
-			<Route path="/work" component={Work} />
-			<Route path="/Contact" component={Contact} />
-		</div>
+		<body>
+			<div className="mobileNotSupported">
+				<header>
+					<h1>모바일 미지원..</h1>
+					<p>
+						아직 모바일 접속이 불가합니다ㅜㅜ
+						<br /> 현재 기능 개발중에 있습니다.
+					</p>
+				</header>
+			</div>
+			<div className="pcInterface">
+				<Route path="/" component={Landing} exact />
+				<Route path="/about" component={About} />
+				<Route path="/skills" component={Skills} />
+				<Route path="/work" component={Work} />
+				<Route path="/Contact" component={Contact} />
+			</div>
+		</body>
 	);
 };
 
